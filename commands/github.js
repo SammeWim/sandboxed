9d68a26ca5d4e348bef09036d0b213304df31c3a
lordjbs <stebojo262@gmail.com>
2017-03-18 18:45:23 +0100

    :D commands/github.js

--- commands/github.js
+++ commands/github.js
@@ -5,7 +5,7 @@ exports.run = function(msg, CommandArguments, Discord, client)
   embed.setColor("#33CCCC");
 
   embed.setTitle("sandboxed: ");
-  embed.addField("Open Source?!", "Yes. I decided to make the bot Open Source, so everyone can see and do their own stuff with sandboxed!", true);
+  embed.addField("Open Source?!", "Yes. I decided to make the bot Open Source, so everyone can see and do their own stuff with sandboxed, and also, help in developing, and maybe make some new commands, that will come in the main bot!", true);
   embed.addField("Github!", "[Click Here](https://github.com/lordjbs/sandboxed)")
 
 
