344a54fc8359e60a46250c2f6589259b5789014b
lordjbs <stebojo262@gmail.com>
2017-03-18 16:44:51 +0100

    added ban beta! commands/ban.js

---
+++ commands/ban.js
@@ -0,0 +1,33 @@
+exports.run = function(msg, args, Discord, client)
+{
+  let reason = args.join(" ");
+  let user = msg.mentions.users.first();
+  let guild = msg.guild;
+  let modlog = msg.guild.channels.find("name", "mod-log")
+
+  if(msg.author.roles.has("name", "Moderator"))
+  {
+    if(reason.length < 1) return msg.reply("You need to add a reason!");
+    if(msg.mentions.users.size < 1) return msg.reply("You need to mention the user you wanna ban!");
+    if(msg.guild.member(user).bannable) return msg.reply("Sorry, but i cannot ban this user!");
+
+    user.sendMessage("You have been banned from " + guild.name + "! Reason: " + reason);
+    msg.guild.ban(user, 2);
+    msg.channel.sendMessage(":ok_hand:");
+
+    if(!modlog)
+    {
+
+    }else{
+      const embed = new Discord.RichEmbed()
+        .setColor(0x00AE86)
+        .setTimestamp()
+        .addField('Action:', 'Ban')
+        .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
+        .addField('Modrator:', `${msg.author.username}#${msg.author.discriminator}`)
+        .addField('Reason', reason);
+      return client.channels.get(modlog.id).sendEmbed(embed);
+    }
+  }else{
+  }
+}
