bad0e114e24b5bdbae49dfa5163c02106fefc826
lordjbs <stebojo262@gmail.com>
2017-03-18 16:54:17 +0100

    Easy errors, and cry xD commands/ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -6,7 +6,7 @@ exports.run = function(msg, args, Discord, client)
   let modlog = msg.guild.channels.find("name", "mod-log")
 
 try{
-  if(msg.author.roles.has("name", "Moderator") || msg.author.id !== msg.guild.owner.id)
+  if(msg.member.roles.has("name", "Moderator") || msg.author.id !== msg.guild.owner.id)
   {
     if(reason.length < 1) return msg.reply("You need to add a reason!");
     if(msg.mentions.users.size < 1) return msg.reply("You need to mention the user you wanna ban!");
