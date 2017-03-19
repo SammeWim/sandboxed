50ca55bcb49e9cf8f6d00a958e2f1f9263e19243
lordjbs <stebojo262@gmail.com>
2017-03-19 08:53:19 +0100

    Cleaned up code & Fixed bug sandboxed.js

--- sandboxed.js
+++ sandboxed.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const config = require("../config.json");
-
+//Insert memes here :P
 const client = new Discord.Client();
 
 let User = client.user;
@@ -13,76 +13,80 @@ client.on("ready", () =>{
 })
 const prefixes = [">>", "sand, ", "sandboxed, "];
 client.on("message", msg =>{
-    if(msg.content.includes(client.token))
+
+  if(msg.content.includes(client.token))
+  {
+    msg.delete();
+    msg.channel.sendMessage("Nice try, bitch.")
+  }
+
+  let prefix = false;
+
+  for(const thisPrefix of prefixes) {
+    if(msg.content.startsWith(thisPrefix)) prefix = thisPrefix;
+  }
+  var command = msg.content.substring(prefix.length).toLowerCase().split(" ")[0];
+  var CommandArguments;
+
+  if(prefix === prefixes[1] || prefix === prefixes[2])
+  {
+    var r = msg.content.split(" ").slice(1);
+    CommandArguments = r.substring(command.length);
+  }else{
+    CommandArguments = msg.content.split(" ").slice(1);
+  }
+
+  if(!msg.content.startsWith(prefix)) return false;
+
+  if(command === "eval")
+  {
+    var permitted = [
+      "220591718158827520"
+    ]
+    if(!permitted.includes(msg.author.id))
     {
-      msg.delete();
-      msg.channel.sendMessage("Nice try, bitch.")
-    }
-    let prefix = false;
-    for(const thisPrefix of prefixes) {
-      if(msg.content.startsWith(thisPrefix)) prefix = thisPrefix;
-    }
-    if (msg.channel.type === 'dm') console.log(msg.author.username + " sended a dm: " + msg.content);
-    var CommandArguments;
-    if(prefix === prefixes[1] || prefix === prefixes[2])
-    {
-      var r = msg.content.split(" ").slice(1);
-      CommandArguments = r.substring(command.length);
+      console.log(msg.author + " was trying to perform eval in " + msg.guild + "!");
     }else{
-      CommandArguments = msg.content.split(" ").slice(1);
-    }
-    var command = msg.content.substring(prefix.length).toLowerCase().split(" ")[0];
-    if(!msg.content.startsWith(prefix)) return false;
-    if(command === "eval")
-    {
-      var permitted = [
-        "220591718158827520"
-      ]
-      if(!permitted.includes(msg.author.id))
+      try{
+        var Code = eval(CommandArguments.join(" "));
+        if (typeof Code !== 'string')
+        Code = require('util').inspect(Code, {depth:0});
+        let tok = client.token
+        Code = Code.replace(new RegExp(client.user.email, "gi"), "git gud").replace(new RegExp(client.token, "gi"), "git gud")
+        var embed = new Discord.RichEmbed();
+        if(!Code.includes(tok.toString()))
+        {
+          embed.setColor("#33CCCC");
+          embed.setTitle("sandboxed - eval");
+          embed.setDescription("```js\n" + Code + "```");
+          embed.setFooter("sandboxed - developed by lordjbs#3049");
+          msg.channel.sendEmbed(embed);
+        }else{
+          msg.delete();
+          msg.channel.sendMessage("Nice try, bitch.");
+        }
+        //msg.channel.sendMessage("```js\n" + Code + "```");
+        console.log("Evaled " + CommandArguments.join(" ") + "! Asked by " + msg.author.username + "! Code/Usage: " + Code);
+      }catch(e)
       {
-          console.log(msg.author + " was trying to perform eval in " + msg.guild + "!");
-      }else{
-          try{
-                   var Code = eval(CommandArguments.join(" "));
-                   if (typeof Code !== 'string')
-                   Code = require('util').inspect(Code, {depth:0});
-                   let tok = client.token
-                   Code = Code.replace(new RegExp(client.user.email, "gi"), "git gud").replace(new RegExp(client.token, "gi"), "git gud")
-                   var embed = new Discord.RichEmbed();
-                   if(!Code.includes(tok.toString()))
-                   {
-                   embed.setColor("#33CCCC");
-                   embed.setTitle("sandboxed - eval");
-                   embed.setDescription("```js\n" + Code + "```");
-                   embed.setFooter("sandboxed - developed by lordjbs#3049");
-                   msg.channel.sendEmbed(embed);
-                 }else{
-                   msg.delete();
-                   msg.channel.sendMessage("Nice try, bitch.");
-                 }
-                   //msg.channel.sendMessage("```js\n" + Code + "```");
-                   console.log("Evaled " + CommandArguments.join(" ") + "! Asked by " + msg.author.username + "! Code/Usage: " + Code);
-
-          }catch(e)
-          {
-            var embed = new Discord.RichEmbed();
-            embed.setTitle("sandboxed - eval - ERROR");
-            embed.setColor("#B9341B");
-            embed.setDescription("```js\n" + e + "```");
-            embed.setFooter("sandboxed - developed by lordjbs#3049");
-            msg.channel.sendEmbed(embed);
-          }
+        var embed = new Discord.RichEmbed();
+        embed.setTitle("sandboxed - eval - ERROR");
+        embed.setColor("#B9341B");
+        embed.setDescription("```js\n" + e + "```");
+        embed.setFooter("sandboxed - developed by lordjbs#3049");
+        msg.channel.sendEmbed(embed);
       }
-    }else if(command === "reboot"){
-      if(msg.author.id !== "220591718158827520"){
+    }
+  }else if(command === "reboot"){
+    if(msg.author.id !== "220591718158827520"){
 
-      }else{
+    }else{
       msg.channel.sendMessage("Rebooting Core...");
       console.log("Rebooting core...")
       setTimeout(reboot, 3000)
       console.log("rebooted!")
     }
-}else{
+  }else{
     var Manager = require("./commands/CommandWrapper.js")
     Manager.performed(msg, command, CommandArguments, Discord, client);
   }
