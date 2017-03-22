3c556f5e196ef7f3b7137dd2a18c8954e35cf6e1
lordjbs <stebojo262@gmail.com>
2017-03-22 19:47:38 +0100

    commands/help.js

--- commands/help.js
+++ commands/help.js
@@ -9,14 +9,13 @@ exports.run = function(message, CommandArguments, Discord, client)
 
   embed.setTitle("sandboxed");
 
-  embed.setDescription("Hey! Thank you for using sandboxed! Here are my Commands:");
-  embed.addField("Commands: ", "^-^", true);
+  embed.setDescription("Hey! Thank you for using sandboxed! Here are my Commands:\n **Commands:**");
   embed.addField("Bot [2]", ">>ping: Showes the ping of the Bot\n>>stats: Showes the Statistics of the bot!", true);
   embed.addField("Misc [2]", ">>github: Gives a info for sandboxed's Github\n>>invite: WHOA! You wanna add me?!")
   embed.addField("Fun [4]", "@sandboxed#9142 [message] - Talk with cleverbot!\n>>gif [query] - Searches after a gif on giphy.com\n>>cat - Gives you a random Cat picture.\n>>yesorno - Says Yes or No. Very easy.", true);
   embed.addField("Music [2]", ">>play <query/link> - Play's Music! \n>>stop - Stops the Music!", true);
   embed.addField("Moderation [4]", ">>purge <messages> - Deletes messages.\n>>ban @<user> - Bans a user!\n>>unban <id> - Unbans a User\n>>kick - @<user> ", true);
-  embed.addField("————————————————————————", "Note: Found a bug, or you wanna have the newest infos of sandboxed? Join sandboxed's guild [Click!](https://discord.gg/ypqHAd9)", true)
+  embed.addField("————————————————————————", "Note: Found a bug, or you wanna have the newest infos of sandboxed? Join sandboxed's guild [Click!](https://discord.gg/ypqHAd9)")
 
   embed.setFooter("sandboxed vS , Coded by @lordjbs#3049");
 
