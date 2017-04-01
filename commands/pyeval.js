35231facbe39ab07b0cbeabefeb66566acc809ec
lordjbs <stebojo262@gmail.com>
2017-04-01 13:57:31 +0200

    commands/pyeval.js

--- commands/pyeval.js
+++ commands/pyeval.js
@@ -3,11 +3,6 @@ exports.run = function(msg, argszz, Discord, client)
   let args = msg.content.split(" ").slice(1);
 
   require("child_process").exec("python3 " + args.join(" "), (e, stdout) => {
-    let embed = new Discord.RichEmbed();
-    embed.setColor("#33CCCC");
-    embed.setTitle("sandboxed - pyeval");
-    embed.setDescription("```\n" + stdout + "```");
-    embed.setFooter("sandboxed - developed by lordjbs#3049");
-    msg.channel.sendEmbed(embed);
+    msg.channel.sendMessage("PyEval: " + stdout)
   })
 };
