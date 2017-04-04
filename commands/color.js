bd08c8b9db8a50efd0e181708095dae6f95476e0
lordjbs <stebojo262@gmail.com>
2017-04-04 17:07:34 +0200

    commands/color.js

--- commands/color.js
+++ commands/color.js
@@ -2,9 +2,8 @@ exports.run = function(msg, args, Discord, client)
 {
   https://projects.festive.tf/color/?color=
 
-  let embed = new Discord.RichEmbed()
-  .setTitle("Color:");
-  .setThumbnail("https://projects.festive.tf/color/?color=" + args[0]);
+  if(args[0].includes)
+  let embed = new Discord.RichEmbed().setTitle("Color:").setThumbnail("https://projects.festive.tf/color/?color=" + args[0]);
 
   msg.channel.sendEmbed(embed);
 }
